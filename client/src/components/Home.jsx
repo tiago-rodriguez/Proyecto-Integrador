81eca0fe130d8ed895e609e1bb7fd94b37f1a829
Titi24Pehuajo <tiagorodriguez58@gmail.com>
2023-03-28 19:11:54 -0300

    Home terminada

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,12 +1,29 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Nav from "react-bootstrap/Nav";
 import "../styles/home.css";
-import axios from "axios";
 
 function Home() {
   return (
-    <div className="home_bg_dos">
-      <div className="home_bg">
-        <div className="border-solid "></div>
+    <div>
+      <div className="home_bg_dos">
+        <h1 className="texto">
+          Hola bienvenido a House of Dev!!! Para una mejor experiencia Ingresa o
+          registrate!!
+          <p></p>
+          <Nav className="me-auto">
+            <Nav.Link href="/login">
+              <Button variant="info">Ingresar</Button>
+            </Nav.Link>
+          </Nav>
+          <Nav className="me-auto">
+            <Nav.Link href="/register">
+              <Button variant="info"> Registrarse </Button>
+            </Nav.Link>
+          </Nav>
+        </h1>
+
+        <div className="home_bg"></div>
       </div>
     </div>
   );
