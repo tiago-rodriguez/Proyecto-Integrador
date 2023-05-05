353f926e667ba94e80c229ec5b075026134699bb
Titi24Pehuajo <tiagorodriguez58@gmail.com>
2023-05-05 16:43:17 -0300

    Ajustes

--- client/src/components/PropertyCreate.jsx
+++ client/src/components/PropertyCreate.jsx
@@ -7,6 +7,7 @@ import { Form } from "react-bootstrap";
 import { message } from "antd";
 
 function UpdateProperty() {
+  const [image, setImage] = useState("");
   const [errors, setErrors] = useState({});
   const [messageApi, contextHolder] = message.useMessage();
   const [newProperty, setNewProperty] = useState({
@@ -15,6 +16,7 @@ function UpdateProperty() {
     adress: "",
     category: "",
     city: "",
+    image: "",
     locate: "",
     country: "",
     operation: "",
@@ -25,10 +27,15 @@ function UpdateProperty() {
     price: 0,
   });
 
+  function handleImage(e) {
+    console.log(e.target.files);
+    setImage(e.target.files[0]);
+  }
+
   const success = () => {
     messageApi.open({
       type: "success",
-      content: "Propiedad Creada",
+      content: "La Propiedad ha sido creada",
     });
   };
 
@@ -45,6 +52,10 @@ function UpdateProperty() {
 
     const token = window.localStorage.getItem("token");
     console.log(newProperty);
+
+    const formData = new FormData();
+    formData.append("image", image);
+
     const newErrors = {};
     if (!newProperty.title) {
       newErrors.title = "Debe ingresar un título";
@@ -59,40 +70,40 @@ function UpdateProperty() {
         "El título debe comenzar con mayúscula la primera letra de cada palabra";
     }
 
-    if (!newProperty.category) {
-      newErrors.category = "Debe seleccionar una categoría";
-    } else if (
-      newProperty.category !== "Casa" &&
-      newProperty.category !== "Departamento"
-    ) {
-      newErrors.category = "La categoría no es válida";
-    }
+    // if (!newProperty.category) {
+    //   newErrors.category = "Debe seleccionar una categoría";
+    // } else if (
+    //   newProperty.category !== "Casa" &&
+    //   newProperty.category !== "Departamento"
+    // ) {
+    //   newErrors.category = "La categoría no es válida";
+    // }
 
-    if (newProperty.description.length < 5) {
-      newErrors.description = "El título debe tener al menos 5 caracteres";
-    } else if (newProperty.description.length > 50) {
-      newErrors.description = "El título debe tener como máximo 50 caracteres";
-    } else if (!/^[A-Z][a-zA-Z\s]*$/.test(newProperty.title)) {
-      newErrors.title =
-        "El título debe comenzar con mayúscula la primera letra de cada palabra";
-    }
+    // if (newProperty.description.length < 5) {
+    //   newErrors.description = "El título debe tener al menos 5 caracteres";
+    // } else if (newProperty.description.length > 50) {
+    //   newErrors.description = "El título debe tener como máximo 50 caracteres";
+    // } else if (!/^[A-Z][a-zA-Z\s]*$/.test(newProperty.title)) {
+    //   newErrors.title =
+    //     "El título debe comenzar con mayúscula la primera letra de cada palabra";
+    // }
 
-    if (!newProperty.operation) {
-      newErrors.operation = "Debe seleccionar una operación";
-    } else if (
-      newProperty.operation !== "Venta" &&
-      newProperty.operation !== "Alquiler"
-    ) {
-      newErrors.operation = "La operación no es válida";
-    }
+    // if (!newProperty.operation) {
+    //   newErrors.operation = "Debe seleccionar una operación";
+    // } else if (
+    //   newProperty.operation !== "Venta" &&
+    //   newProperty.operation !== "Alquiler"
+    // ) {
+    //   newErrors.operation = "La operación no es válida";
+    // }
 
-    if (!newProperty.enviroments) {
-      newErrors.enviroments = "Debe seleccionar algún valor";
-    }
+    // if (!newProperty.enviroments) {
+    //   newErrors.enviroments = "Debe seleccionar algún valor";
+    // }
 
-    if (!newProperty.bathrooms) {
-      newErrors.bathrooms = "Debe seleccionar algún valor";
-    }
+    // if (!newProperty.bathrooms) {
+    //   newErrors.bathrooms = "Debe seleccionar algún valor";
+    // }
 
     // Validar los demás campos del formulario
     // ...
@@ -104,7 +115,9 @@ function UpdateProperty() {
       axios
         .post(
           "http://localhost:3001/api/properties/create",
-
+          // formData.then((res) => {
+          //   console.log(res);
+          // }),
           {
             newProperty,
 
@@ -119,8 +132,8 @@ function UpdateProperty() {
         });
       messageApi
         .open({
-          type: "success",
-          content: "La propiedad ha sido creada",
+          // type: "success",
+          // content: "La propiedad ha sido creada",
         })
         .catch((error) => {
           // Hubo un error al enviar la solicitud
@@ -134,195 +147,215 @@ function UpdateProperty() {
       {contextHolder}
       <div>
         <h1 className="text">Crea tu propiedad!!</h1>
-        <form className="formulario" onSubmit={handleSubmit}>
-          <br></br>
+        <div className="d-flex justify-content-center">
+          <form className="formulario" onSubmit={handleSubmit}>
+            <Form.Group controlId="title">
+              <Form.Label>Titulo:</Form.Label>
+              <Form.Control
+                name="title"
+                type="text"
+                required
+                maxLength={100}
+                value={newProperty.title}
+                onChange={handleInput}
+              />
+            </Form.Group>
+            {errors.title && <span className="error">{errors.title}</span>}
+            <br></br>
+            <Form.Group controlId="description">
+              <Form.Label>Descripción:</Form.Label>
+              <Form.Control
+                name="description"
+                as="textarea"
+                rows={3}
+                required
+                maxLength={100}
+                value={newProperty.description}
+                onChange={handleInput}
+              />
+            </Form.Group>
+            <br></br>
+            <Form.Group controlId="adress">
+              <Form.Label>Direccion:</Form.Label>
+              <Form.Control
+                name="adress"
+                type="text"
+                required
+                maxLength={50}
+                value={newProperty.adress}
+                onChange={handleInput}
+              />
+            </Form.Group>
+            <br></br>
 
-          <Form.Group className="form-group-custom" controlId="title">
-            <Form.Label>Titulo:</Form.Label>
-            <Form.Control
-              name="title"
-              type="text"
-              required
-              maxLength={100}
-              value={newProperty.title}
-              onChange={handleInput}
-            />
-          </Form.Group>
-          {errors.title && <span className="error">{errors.title}</span>}
-          <br></br>
-          <Form.Group className="form-group-custom" controlId="adress">
-            <Form.Label>Direccion:</Form.Label>
-            <Form.Control
-              name="adress"
-              type="text"
-              required
-              maxLength={50}
-              value={newProperty.adress}
-              onChange={handleInput}
-            />
-          </Form.Group>
-          <br></br>
+            <Form.Group controlId="country">
+              <Form.Label>Pais:</Form.Label>
+              <Form.Control
+                name="country"
+                type="text"
+                maxLength={50}
+                required
+                value={newProperty.country}
+                onChange={handleInput}
+              />
+            </Form.Group>
 
-          <Form.Group className="form-group-custom" controlId="country">
-            <Form.Label>Pais:</Form.Label>
-            <Form.Control
-              name="country"
-              type="text"
-              maxLength={50}
-              required
-              value={newProperty.country}
-              onChange={handleInput}
-            />
-          </Form.Group>
+            <Form.Group controlId="city">
+              <Form.Label>Ciudad:</Form.Label>
+              <Form.Control
+                name="city"
+                type="text"
+                maxLength={50}
+                required
+                value={newProperty.city}
+                onChange={handleInput}
+              />
+            </Form.Group>
+            <br></br>
+            <Form.Group controlId="Locate">
+              <Form.Label>Barrio:</Form.Label>
+              <Form.Control
+                name="locate"
+                type="text"
+                maxLength={50}
+                required
+                value={newProperty.locate}
+                onChange={handleInput}
+              />
+            </Form.Group>
 
-          <Form.Group className="form-group-custom" controlId="city">
-            <Form.Label>Ciudad:</Form.Label>
-            <Form.Control
-              name="city"
-              type="text"
-              maxLength={50}
-              required
-              value={newProperty.city}
-              onChange={handleInput}
+            <input
+              type="file"
+              name="file"
+              accept="image/png, image/gif, image/jpeg"
+              onChange={handleImage}
             />
-          </Form.Group>
-          <br></br>
-          <Form.Group className="form-group-custom" controlId="Locate">
-            <Form.Label>Barrio:</Form.Label>
-            <Form.Control
-              name="locate"
-              type="text"
-              maxLength={50}
-              required
-              value={newProperty.locate}
-              onChange={handleInput}
-            />
-          </Form.Group>
-          <br></br>
-          <Form.Group className="form-group-custom" controlId="category">
-            <Form.Label>Categoría:</Form.Label>
-            <Form.Select
-              name="category"
-              value={newProperty.category}
-              onChange={handleInput}
-              isInvalid={!!errors.category}
-              required
-            >
-              <option value="">Seleccionar categoría...</option>
-              <option value="Casa">Casa</option>
-              <option value="Departamento">Departamento</option>
-            </Form.Select>
-            <Form.Control.Feedback type="invalid">
-              {errors.category}
-            </Form.Control.Feedback>
-          </Form.Group>
-          <br></br>
-          <Form.Group className="form-group-custom" controlId="operation">
-            <Form.Label>Operacion:</Form.Label>
-            <Form.Select
-              name="operation"
-              value={newProperty.operation}
-              onChange={handleInput}
-              isInvalid={!!errors.operation}
-              required
-            >
-              <option value="">Seleccionar categoría...</option>
-              <option value="Venta">Venta</option>
-              <option value="Alquiler">Alquiler</option>
-            </Form.Select>
-            <Form.Control.Feedback type="invalid">
-              {errors.operation}
-            </Form.Control.Feedback>
-          </Form.Group>
-          <br></br>
-          <Form.Group className="form-group-custom" controlId="enviroments">
-            <Form.Label>Ambientes:</Form.Label>
-            <Form.Select
-              name="enviroments"
-              value={newProperty.enviroments}
-              onChange={handleInput}
-              isInvalid={!!errors.enviroments}
-              required
-            >
-              <option value="">Seleccionar ambientes...</option>
-              <option value={1}>1</option>
-              <option value={2}>2</option>
-              <option value={3}>3</option>
-              <option value={4}>4</option>
-            </Form.Select>
-            <Form.Control.Feedback type="invalid">
-              {errors.enviroments}
-            </Form.Control.Feedback>
-          </Form.Group>
-          <br></br>
-          <Form.Group className="form-group-custom" controlId="bathrooms">
-            <Form.Label>Baños:</Form.Label>
-            <Form.Select
-              name="bathrooms"
-              value={newProperty.bathrooms}
-              onChange={handleInput}
-              isInvalid={!!errors.bathrooms}
-              required
-            >
-              <option value="">Seleccionar baños...</option>
-              <option value={1}>1</option>
-              <option value={2}>2</option>
-              <option value={3}>3</option>
-            </Form.Select>
-            <Form.Control.Feedback type="invalid">
-              {errors.enviroments}
-            </Form.Control.Feedback>
-          </Form.Group>
-          <br></br>
-          <Form.Group className="form-group-custom" controlId="rooms">
-            <Form.Label>Habitaciones:</Form.Label>
-            <Form.Select
-              name="rooms"
-              value={newProperty.rooms}
-              onChange={handleInput}
-              isInvalid={!!errors.rooms}
-              required
-            >
-              <option value="">Seleccionar habitaciones...</option>
-              <option value={1}>1</option>
-              <option value={2}>2</option>
-              <option value={3}>3</option>
-            </Form.Select>
-            <Form.Control.Feedback type="invalid">
-              {errors.enviroments}
-            </Form.Control.Feedback>
-          </Form.Group>
-          <br></br>
-          <Form.Group className="form-group-custom" controlId="meters">
-            <Form.Label>Metros:</Form.Label>
-            <Form.Control
-              name="meters"
-              type="number"
-              required
-              maxLength={10}
-              value={newProperty.meters}
-              onChange={handleInput}
-            />
-          </Form.Group>
-          <br></br>
 
-          <Form.Group className="form-group-custom" controlId="price">
-            <Form.Label>Precio:</Form.Label>
-            <Form.Control
-              name="price"
-              type="number"
-              required
-              value={newProperty.price}
-              onChange={handleInput}
-            />
-          </Form.Group>
-          <br></br>
+            <br></br>
+            <Form.Group controlId="category">
+              <Form.Label>Categoría:</Form.Label>
+              <Form.Select
+                name="category"
+                value={newProperty.category}
+                onChange={handleInput}
+                isInvalid={!!errors.category}
+                required
+              >
+                <option value="">Seleccionar categoría...</option>
+                <option value="Casa">Casa</option>
+                <option value="Departamento">Departamento</option>
+              </Form.Select>
+              <Form.Control.Feedback type="invalid">
+                {errors.category}
+              </Form.Control.Feedback>
+            </Form.Group>
+            <br></br>
+            <Form.Group controlId="operation">
+              <Form.Label>Operacion:</Form.Label>
+              <Form.Select
+                name="operation"
+                value={newProperty.operation}
+                onChange={handleInput}
+                isInvalid={!!errors.operation}
+                required
+              >
+                <option value="">Seleccionar categoría...</option>
+                <option value="Venta">Venta</option>
+                <option value="Alquiler">Alquiler</option>
+              </Form.Select>
+              <Form.Control.Feedback type="invalid">
+                {errors.operation}
+              </Form.Control.Feedback>
+            </Form.Group>
+            <br></br>
+            <Form.Group controlId="enviroments">
+              <Form.Label>Ambientes:</Form.Label>
+              <Form.Select
+                name="enviroments"
+                value={newProperty.enviroments}
+                onChange={handleInput}
+                isInvalid={!!errors.enviroments}
+                required
+              >
+                <option value="">Seleccionar ambientes...</option>
+                <option value={1}>1</option>
+                <option value={2}>2</option>
+                <option value={3}>3</option>
+                <option value={4}>4</option>
+              </Form.Select>
+              <Form.Control.Feedback type="invalid">
+                {errors.enviroments}
+              </Form.Control.Feedback>
+            </Form.Group>
+            <br></br>
+            <Form.Group controlId="bathrooms">
+              <Form.Label>Baños:</Form.Label>
+              <Form.Select
+                name="bathrooms"
+                value={newProperty.bathrooms}
+                onChange={handleInput}
+                isInvalid={!!errors.bathrooms}
+                required
+              >
+                <option value="">Seleccionar baños...</option>
+                <option value={1}>1</option>
+                <option value={2}>2</option>
+                <option value={3}>3</option>
+              </Form.Select>
+              <Form.Control.Feedback type="invalid">
+                {errors.enviroments}
+              </Form.Control.Feedback>
+            </Form.Group>
+            <br></br>
+            <Form.Group controlId="rooms">
+              <Form.Label>Habitaciones:</Form.Label>
+              <Form.Select
+                name="rooms"
+                value={newProperty.rooms}
+                onChange={handleInput}
+                isInvalid={!!errors.rooms}
+                required
+              >
+                <option value="">Seleccionar habitaciones...</option>
+                <option value={1}>1</option>
+                <option value={2}>2</option>
+                <option value={3}>3</option>
+              </Form.Select>
+              <Form.Control.Feedback type="invalid">
+                {errors.enviroments}
+              </Form.Control.Feedback>
+            </Form.Group>
+            <br></br>
+            <Form.Group controlId="meters">
+              <Form.Label>Metros:</Form.Label>
+              <Form.Control
+                name="meters"
+                type="number"
+                required
+                maxLength={10}
+                value={newProperty.meters}
+                onChange={handleInput}
+              />
+            </Form.Group>
+            <br></br>
 
-          <button class="btn btn-info" type="submit">
-            Guardar
-          </button>
-        </form>
+            <Form.Group controlId="price">
+              <Form.Label>Precio:</Form.Label>
+              <Form.Control
+                name="price"
+                type="number"
+                required
+                value={newProperty.price}
+                onChange={handleInput}
+              />
+            </Form.Group>
+            <br></br>
 
+            <button class="btn btn-info" type="submit">
+              Guardar
+            </button>
+          </form>
+        </div>
         <Nav>
           <Nav.Link href="/properties">
             <button type="button" class="btn btn-info">
